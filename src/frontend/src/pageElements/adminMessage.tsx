f050c8eb8051ee850c3caa8e72f03e22da98f4fd
Rob Zeeman <rob.zeeman@di.huc.knaw.nl>
2026-08-24 21:26:53 +0200

    Messaging completed

--- src/frontend/src/pageElements/adminMessage.tsx
+++ src/frontend/src/pageElements/adminMessage.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import {useState, useEffect} from "react";
+import {API_URL} from "../misc/functions";
 
 function AdminMessage() {
-    const [message, setMessage] = useState("Je veter zit los!");
+    const [message, setMessage] = useState("");
     const [showMessage, setShowMessage] = useState(true);
+    const [activateTimer, setActivateTimer] = useState(true);
+    const [loading, setLoading] = useState(true);
+
+    async function get_message() {
+        const response = await fetch(API_URL + 'get_message');
+        const json = await response.json();
+        if (json.message !== message) {
+            setMessage(json.message);
+            setShowMessage(true);
+        }
+        setLoading(false);
+        setActivateTimer(!activateTimer);
+    }
+
+    useEffect(() => {
+        if (loading) get_message()
+            else setTimeout(get_message, 120000);
+    }, [activateTimer]);
 
     return(
         <>
-            {message !== "" && showMessage && <div className="userMessage">{message}<div className="userMessageSwitchOff">Close</div></div>}
+            {message !== "" && showMessage && <div className="userMessage">{message}<div className="userMessageSwitchOff" onClick={() => setShowMessage(false)}>Close (x)</div></div>}
         </>
     )
 }
